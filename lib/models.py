3aed02577dd15b8409a75464121451f2a598e403
edmond <edmond.wanyama@student.moringaschool.com>
2023-09-03 01:30:29 +0300

    Table with customer base

--- lib/models.py
+++ lib/models.py
@@ -17,3 +17,10 @@ customer_restaurants = Table(
     Column("restaurant_id", ForeignKey("restaurants.id"), primary_key=True),
     extend_existing=True,
 )
+
+# ....................customers class....................
+class Customer(Base):
+    __tablename__ = 'customers'
+    id = Column(Integer(), primary_key=True)
+    first_name = Column(String())
+    last_name = Column(String())
